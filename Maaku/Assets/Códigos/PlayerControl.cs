cdbc4165d66c269738925bd64cbdfa0a7832d479
alamgro <alamgrodriguez@gmail.com>
2020-04-10 16:32:59 -0500

    Intro del juego
    Se añade una escena nueva que es la intro del juego con una pantalla en negro donde pondrá el texto introductorio.
    Se puede saltar este intro con la tecla SPACE.
    Se eliminan animaciones viejas del personaje.

--- Maaku/Assets/Códigos/PlayerControl.cs
+++ Maaku/Assets/Códigos/PlayerControl.cs
@@ -4,47 +4,8 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-
     public float moveSpeed = 4.0f, jumpSpeed = 10.0f;
-    bool isJumping = false;
-    /*Animator anim;
     
-    void Start()
-    {
-        //gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
-        anim = GetComponent<Animator>();
-    }
-
-    void FixedUpdate()
-    {
-        Movement();
-
-        float move = Input.GetAxis("Horizontal");
-        anim.SetFloat("Speed", move);
-    }
-
-    void Movement()
-    {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector2.up * jumpSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-    }
-   
-    */
-
     void Update()
     {
         if (Input.GetKey(KeyCode.D)) //Caminar a la derecha
@@ -64,18 +25,11 @@ public class PlayerControl : MonoBehaviour
             transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.W) ) { //Brincar
+        if (Input.GetKey(KeyCode.W) ) //Brincar
             transform.Translate(Vector2.up * jumpSpeed * Time.deltaTime);
-            isJumping = true;
-        }
 
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) //Verificar cuando deja de caminar
             GetComponent<Animator>().SetBool("caminar", false);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.name == "Background")
-            isJumping = false;
-    }
 }
